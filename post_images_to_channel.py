9526b6b6070c5f3a45b30c20dd23ea21e2b7103c
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-11-08 19:18:32 +0200

    add telegram bot

--- post_images_to_channel.py
+++ post_images_to_channel.py
@@ -1,23 +1,12 @@
 import argparse
 import time
 import os
-import telegram
 from dotenv import load_dotenv
 import random
+from telegram_bot import send_file
 
 load_dotenv()
 
-telegram_api_key = os.environ["TOKEN"]
-chat_id = os.environ['CHAT_ID']
-bot = telegram.Bot(token=telegram_api_key)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-delay_time", type=int, default=14400, help="Интервал отправки изображений")
-group = parser.add_mutually_exclusive_group(required=False)
-group.add_argument("--image_name", help="Название файла с расширением")
-group.add_argument("--infinity_loop", action='store_true', help="Запуск бесконечного цикла отправки изображений")
-args = parser.parse_args()
-print(args)
 
 nasa_images = []
 for file in os.walk("images"):
@@ -28,35 +17,47 @@ for file in os.walk("images"):
 def send_images(delay):
     for image in nasa_images:
         file_path = os.path.join("images", image)
-        bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
+        send_file(file_path)
         time.sleep(delay)
 
 
 def send_random_image():
     random_image = random.choice(nasa_images)
-    print(random_image)
     file_path = os.path.join("images", random_image)
-    bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
+    send_file(file_path)
 
 
 def send_one_image(image_name):
     file_path = os.path.join("images", image_name)
-    bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
+    send_file(file_path)
 
 
-if args.image_name is None and args.infinity_loop is False:
-    print('random')
-    send_random_image()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-delay_time", type=int, default=14400, help="Интервал отправки изображений")
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("--image_name", help="Название файла с расширением")
+    group.add_argument("--infinity_loop", action='store_true', help="Запуск бесконечного цикла отправки изображений")
+    args = parser.parse_args()
 
-if args.image_name is not None:
-    print('image_name')
-    send_one_image(args.image_name)
+    if args.image_name is None and args.infinity_loop is False:
+        send_random_image()
+        return
 
-if args.infinity_loop is not None:
-    print('infinity')
-    while True:
-        send_images(args.delay_time)
-        random.shuffle(nasa_images)
+    if args.image_name is not None:
+        send_one_image(args.image_name)
+        return
+
+    if args.infinity_loop is not None:
+        while True:
+            send_images(args.delay_time)
+            random.shuffle(nasa_images)
+            return
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 
---
+++ telegram_bot.py
@@ -0,0 +1,14 @@
+import telegram
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_file(file_path):
+    telegram_api_key = os.environ["TOKEN"]
+    chat_id = os.environ['CHAT_ID']
+    bot = telegram.Bot(token=telegram_api_key)
+    bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
+
+
