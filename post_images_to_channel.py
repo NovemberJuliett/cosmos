7b6f049ea909e8f559ebdf93672f574352d61cb9
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-11-08 16:29:06 +0200

    edit post_images_to_channel

--- post_images_to_channel.py
+++ post_images_to_channel.py
@@ -7,6 +7,17 @@ import random
 
 load_dotenv()
 
+telegram_api_key = os.environ["TOKEN"]
+chat_id = os.environ['CHAT_ID']
+bot = telegram.Bot(token=telegram_api_key)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-delay_time", type=int, default=14400, help="Интервал отправки изображений")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--image_name", help="Название файла с расширением")
+group.add_argument("--infinity_loop", help="Запуск бесконечного цикла отправки изображений")
+args = parser.parse_args()
+print(args)
 
 nasa_images = []
 for file in os.walk("images"):
@@ -14,31 +25,36 @@ for file in os.walk("images"):
         nasa_images.append(element)
 
 
-def send_image(delay):
+def send_images(delay):
     for image in nasa_images:
         file_path = os.path.join("images", image)
         bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
         time.sleep(delay)
 
 
-def random_image():
+def send_random_image():
     random.shuffle(nasa_images)
 
 
-def main():
-    telegram_api_key = os.environ["TOKEN"]
-    chat_id = os.environ['CHAT_ID']
-    bot = telegram.Bot(token=telegram_api_key)
+def send_one_image(image_name):
+    file_path = os.path.join("images", image_name)
+    bot.send_document(chat_id=chat_id, document=open(file_path, "rb"))
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-delay_time", type=int, default=14400, help="Интервал отправки изображений")
-    args = parser.parse_args()
 
+if args.image_name is None and args.infinity_loop is None:
+    print('random')
+    send_random_image()
+
+if args.image_name is not None:
+    print('image_name')
+    send_one_image(args.image_name)
+
+if args.infinity_loop is not None:
+    print('loop')
     while True:
-        send_image(args.delay_time)
-        random_image()
+        send_images(args.delay_time)
+        send_random_image()
 
 
-if __name__ == '__main__':
-    main()
+
 
